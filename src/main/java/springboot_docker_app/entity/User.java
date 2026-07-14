b07a36f27b510d0e8140727bc7b0987797f47e20
apramgupta26 <apramgupta261105@gmail.com>
2026-07-14 08:30:25 +0530

    Update User.java

--- src/main/java/springboot_docker_app/entity/User.java
+++ src/main/java/springboot_docker_app/entity/User.java
@@ -1,30 +1,45 @@
 package springboot_docker_app.entity;
 
+
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    // Default constructor (required by JPA)
     public User() {
     }
 
-    public User(Long id, String name) {
-        this.id = id;
+    public User(String name) {
         this.name = name;
     }
 
+    // Getters and setters
     public Long getId() {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
